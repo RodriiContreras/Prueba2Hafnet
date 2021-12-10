a00e4b112ce4b4d3b6145b2967e6909b1eab1656
unknown <rodrigolcontreras1317@gmail.com>
2021-12-09 23:57:42 -0300

    agregamos scrollreveal

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+
+ScrollReveal().reveal('.navv',{
+       duration:800,
+       origin:'bottom',
+       distance:'-100px'
+   
+  });
+  
+   ScrollReveal().reveal('.contenedorcarrusel',{
+    duration:2000,
+   
+   
+  });
+  
+  ScrollReveal().reveal('.presentacion__titulo',{
+    duration:2000,
+    origin:'right',
+    distance:'-700px'
+
+   
+   
+  });
+  
+  
+  ScrollReveal().reveal('.presentacion__empresa',{
+    duration:4000
+  });
+  ScrollReveal().reveal('.card',{
+    duration:3000,
+    origin:'top',
+    distance:'-400px'
+  });
